9f40c4dc4d4b29a745c184e3888fd525913671d4
OSadowski <101937089+OSadowski@users.noreply.github.com>
2024-09-11 20:14:01 +0100

    Update ChessEngine.py
    Added Valid moves for white and black pawns

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -16,6 +16,8 @@ class GameState():
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
+        self.moveFunctions = {"p": self.getPawnMoves, "R": self.getRookMoves, "N": self.getKnightMoves,
+                              "B": self.getBishopMoves, "Q": self.getQueenMoves, "K": self.getKingMoves}
         self.whiteToMove = True
         self.moveLog = []
 
@@ -32,8 +34,61 @@ class GameState():
             self.board[move.endRow][move.endCol] = move.pieceCaptured
             self.whiteToMove = not self.whiteToMove
 
-class Move():
+    def getValidMoves(self):
+        return self.getAllPossibleMoves()
 
+    def getAllPossibleMoves(self):
+        moves = []
+        for row in range(len(self.board)):
+            for col in range(len(self.board[row])):
+                turn = self.board[row][col][0]
+                if (turn == "w" and self.whiteToMove) or (turn == "b" and not self.whiteToMove):
+                    piece = self.board[row][col][1]
+                    self.moveFunctions[piece](row, col, moves)
+        return moves
+
+    def getPawnMoves(self, row, col, moves):
+        if self.whiteToMove:
+            if self.board[row - 1][col] == "--":
+                moves.append(Move((row, col), (row - 1, col), self.board))
+                if row == 6 and self.board[row - 2][col] == "--":
+                    moves.append(Move((row, col), (row - 2, col), self.board))
+            if col - 1 >= 0:  # Captures to the Left
+                if self.board[row - 1][col - 1][0] == "b":
+                    moves.append(Move((row, col), (row - 1, col - 1), self.board))
+            if col + 1 <= 7:  # Captures to the Right
+                if self.board[row - 1][col + 1][0] == "b":
+                    moves.append(Move((row, col), (row - 1, col + 1), self.board))
+        else:
+            if self.board[row + 1][col] == "--":
+                moves.append(Move((row, col), (row + 1, col), self.board))
+                if row == 1 and self.board[row + 2][col] == "--":
+                    moves.append(Move((row, col), (row + 2, col), self.board))
+            if col - 1 >= 0:  # Captures to the Left
+                if self.board[row + 1][col - 1][0] == "w":
+                    moves.append(Move((row, col), (row + 1, col - 1), self.board))
+            if col + 1 <= 7:  # Captures to the Right
+                if self.board[row + 1][col + 1][0] == "w":
+                    moves.append(Move((row, col), (row + 1, col + 1), self.board))
+
+
+    def getRookMoves(self, row, col, moves):
+        pass
+
+    def getKnightMoves(self, row, col, moves):
+        pass
+
+    def getBishopMoves(self, row, col, moves):
+        pass
+
+    def getQueenMoves(self, row, col, moves):
+        pass
+
+    def getKingMoves(self, row, col, moves):
+        pass
+
+
+class Move():
     ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
                    "5": 3, "6": 2, "7": 1, "8": 0}
     rowsToRanks = {v: k for k, v in ranksToRows.items()}
@@ -48,6 +103,11 @@ class Move():
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
 
     def getChessNotation(self):
         return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
