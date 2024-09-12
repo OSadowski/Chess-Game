b5a679d36bfae4dbfae83027ea7ece41cfde1648
OSadowski <101937089+OSadowski@users.noreply.github.com>
2024-09-12 19:11:19 +0100

    Update ChessEngine.py
    Added function to add valid Bishop moves, same as Rook just changed directions

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -97,7 +97,23 @@ class GameState():
         pass
 
     def getBishopMoves(self, row, col, moves):
-        pass
+        directions = ((-1, -1), (-1, 1), (1, -1), (1, 1))
+        enemyColour = "b" if self.whiteToMove else "w"
+        for d in directions:
+            for i in range(1, 8):
+                endRow = row + d[0] * i
+                endCol = col + d[1] * i
+                if 0 <= endRow < 8 and 0 <= endCol < 8:
+                    endPiece = self.board[endRow][endCol]
+                    if endPiece == "--":
+                        moves.append(Move((row, col), (endRow, endCol), self.board))
+                    elif endPiece[0] == enemyColour:
+                        moves.append(Move((row, col), (endRow, endCol), self.board))
+                        break
+                    else:
+                        break
+                else:
+                    break
 
     def getQueenMoves(self, row, col, moves):
         pass
