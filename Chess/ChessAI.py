430976b3077fee3a5187699039dc4dfb20eadcfd
Oskar Sadowski <101937089+OSadowski@users.noreply.github.com>
2024-09-30 19:50:14 +0100

    AI moves now make semi-good moves
    depth of 2 moves

--- Chess/ChessAI.py
+++ Chess/ChessAI.py
@@ -1,4 +1,50 @@
 import random
 
+pieceScore = {"K": 0, "Q": 9, "R": 5, "B": 3, "N": 3, "p": 1}
+checkMateScore = 1000
+staleMateScore = 0
+
+
 def findRandomMove(validMoves):
     return validMoves[random.randint(0, len(validMoves)-1)]
+
+
+def findBestMove(gameState, validMoves):
+
+    turnMultiplier = 1 if gameState.white_to_move else -1
+    opponentMinMaxScore = checkMateScore
+    bestMove = None
+    random.shuffle(validMoves)
+
+    for currentMove in validMoves:
+        gameState.makeMove(currentMove)
+        opponentsMoves = gameState.getValidMoves()
+        opponentsMaxScore = -checkMateScore
+
+        for opponentsMoves in opponentsMoves:
+            gameState.makeMove(opponentsMoves)
+            if gameState.check_mate:
+                score = -turnMultiplier * checkMateScore
+            elif gameState.stale_mate:
+                score = staleMateScore
+            else:
+                score = -turnMultiplier * scoreMaterial(gameState.board)
+            if score > opponentsMaxScore:
+                opponentsMaxScore = score
+            gameState.undoMove()
+        if opponentsMaxScore < opponentMinMaxScore:
+            opponentMinMaxScore = opponentsMaxScore
+            bestMove = currentMove
+        gameState.undoMove()
+    return bestMove
+
+
+def scoreMaterial(board):
+    score = 0
+    for row in board:
+        for square in row:
+            if square[0] == "w":
+                score += pieceScore[square[1]]
+            elif square[0] == "b":
+                score += pieceScore[square[1]]
+    return score
