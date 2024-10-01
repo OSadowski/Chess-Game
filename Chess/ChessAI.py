6aef4a63984961709e486ca87b88e8cce43c9082
Oskar Sadowski <101937089+OSadowski@users.noreply.github.com>
2024-10-01 14:42:33 +0100

    Same Algo, More efficient
    Added recursion and depth

--- Chess/ChessAI.py
+++ Chess/ChessAI.py
@@ -3,45 +3,62 @@ import random
 pieceScore = {"K": 0, "Q": 9, "R": 5, "B": 3, "N": 3, "p": 1}
 checkMateScore = 1000
 staleMateScore = 0
+Depth = 3
 
 
 def findRandomMove(validMoves):
     return validMoves[random.randint(0, len(validMoves)-1)]
 
 
-def findBestMove(gameState, validMoves):
+def findBestMoveMinMax(gameState, validMoves):
+    global nextMove
+    findMoveMinMax(gameState, validMoves, Depth, gameState.white_to_move)
 
-    turnMultiplier = 1 if gameState.white_to_move else -1
-    opponentMinMaxScore = checkMateScore
-    bestMove = None
-    random.shuffle(validMoves)
+    return nextMove
 
-    for currentMove in validMoves:
-        gameState.makeMove(currentMove)
-        opponentsMoves = gameState.getValidMoves()
-        opponentsMaxScore = -checkMateScore
 
-        for opponentsMoves in opponentsMoves:
-            gameState.makeMove(opponentsMoves)
-            if gameState.check_mate:
-                score = -turnMultiplier * checkMateScore
-            elif gameState.stale_mate:
-                score = staleMateScore
-            else:
-                score = -turnMultiplier * scoreMaterial(gameState.board)
-            if score > opponentsMaxScore:
-                opponentsMaxScore = score
+def  findMoveMinMax(gameState, validMoves, depth, whiteToMove):
+    global nextMove
+    if depth == 0:
+        return scoreBoard(gameState.board)
+
+    if whiteToMove:
+        maxScore = -checkMateScore
+        for move in validMoves:
+            gameState.makeMove(move)
+            nextMoves = gameState.getValidMoves()
+            score = findMoveMinMax(gameState, nextMoves, depth-1, False)
+            if score > maxScore:
+                maxScore = score
+                if depth == Depth:
+                    nextMove = move
             gameState.undoMove()
-        if opponentsMaxScore < opponentMinMaxScore:
-            opponentMinMaxScore = opponentsMaxScore
-            bestMove = currentMove
-        gameState.undoMove()
-    return bestMove
+        return maxScore
+    else:
+        minScore = checkMateScore
+        for move in validMoves:
+            gameState.makeMove(move)
+            nextMoves = gameState.getValidMoves()
+            score = findMoveMinMax(gameState, nextMoves, depth-1, True)
+            if score < minScore:
+                minScore = score
+                if depth == Depth:
+                    nextMove = move
+            gameState.undoMove()
+        return minScore
 
 
-def scoreMaterial(board):
+def scoreBoard(gameState):
+    if gameState.check_mate:
+        if gameState.white_to_move:
+            return -checkMateScore
+        else:
+            return checkMateScore
+    elif gameState.stale_mate:
+        return staleMateScore
+
     score = 0
-    for row in board:
+    for row in gameState.board:
         for square in row:
             if square[0] == "w":
                 score += pieceScore[square[1]]
